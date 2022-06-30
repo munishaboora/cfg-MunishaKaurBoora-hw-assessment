6a1ac56ee49635ee75b371bb8d5f4be77a69e36b
munishaboora <munishabora@hotmail.co.uk>
2022-07-01 00:54:34 +0100

    First half of Q1

---
+++ MunishaKaurBoora_HW5.py
@@ -0,0 +1,53 @@
+"""Homework 5-6"""
+"""TASK 1 (Agile Techniques)"""
+###Question 1###
+"""
+SCRUM CEREMONIES
+· Product backlog refinement
+   - Product Backlog refinement is the act of adding detail, estimates, and order to items in the Product Backlog. 
+   - This is an ongoing process in which the Product Owner and the Development Team come together to discuss the details of the items in the Product Backlog. 
+   - During Product Backlog refinement, items are reviewed and revised. 
+   - The product backlog is essentially a list of improvements to be made to one's product.
+
+· Sprint planning 
+   - Sprint planning id an event in scrum where the team determines the items to be in the product backlog which they will work on during the upcoming sprint.  
+   - Plans for how the items in the product backlog will be completed are also discussed.
+   
+· Daily scrum
+   - A daily scrum meeting is a 15-minute event for the developers of the Scrum Team.
+   - The meeting typically takes place at the same time and place every working day of the Sprint.
+   - The purpose of the Daily Scrum meeting is to ensure progress towards the Sprint Goal.
+   - In the daily scrum meeting the development team would produce an actionable plan for the next working day. 
+   
+· Sprint review
+   - The sprint review is a Scrum event that takes place at the end of the sprint (but before the retrospective). 
+   - The purpose of the Sprint Review is for the scrum team to show what they have accomplished during the sprint to key stakeholders.
+   - The sprint review is also used to determine whether or not any alterations are to be made to the product in the future. 
+   
+· Sprint retrospective
+   - The sprint retrospective is a meeting held at the end of a sprint -  it is usually the last thing done in a sprint.
+      - Many teams will hold a sprint retrospective it immediately after the sprint review.
+   - The purpose of the Sprint Retrospective is to plan ways to increase quality and effectiveness.
+      - It is an opportunity for the Scrum Team to reflect and plan improvements that should be made during the next Sprint.
+   - The Scrum Team discusses would normally discuss what went well during the Sprint, the problems that were encountered, and how those problems were resolved (if they were).
+
+SCRUM ROLES
+· Scrum Master
+   - 
+· Product Owner
+   -
+· Development Team
+   -
+"""
+
+###Question 2###
+"""
+User stories:
+- 
+- 
+- 
+
+"""
+
+"""TASK 2 (Process Flow Design)"""
+###Question 1###
