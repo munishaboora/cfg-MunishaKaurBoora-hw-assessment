a4fb235d987d4c713642a14a9da41320923e8237
munishaboora <munishabora@hotmail.co.uk>
2022-07-30 03:23:45 +0100

    Sample code written

--- MunishaKaurBoora_Specialisation_HW3.py
+++ MunishaKaurBoora_Specialisation_HW3.py
@@ -1,29 +1,20 @@
-
 def generate_phrase(characters, phrase):
+    phrase_index = 1
+    for letter in phrase:
+        print(letter)
+
+        # if phrase[:phrase_index] in characters:
+        #     index = characters.find(phrase[:phrase_index])
+        #     characters = characters[:index] + characters[index+1:]
+        #
+        #     phrase = phrase[:phrase_index-1] + phrase[phrase_index:]
+        #     print(phrase)
+            # phrase_index -= 1
+
+        # phrase_index += 1
 
 
-
-
-# def generate_phrase(characters, phrase):
-#     sorted_phrase = sorted(phrase)
-#     phrase_index = 1
-#     for letter in phrase:
-#
-#         if phrase[:phrase_index] in characters:
-#             index = characters.find(phrase[:phrase_index])
-#             characters = characters[:index] + characters[index+1:]
-#
-#             phrase = phrase[:phrase_index-1] + phrase[phrase_index:]
-#             print(phrase)
-#             phrase_index -= 1
-#         continue
-#         phrase_index += 1
-#
-#
-#         return phrase in characters
-
-
-
+        # return phrase in characters
 
 ###################################################
 
