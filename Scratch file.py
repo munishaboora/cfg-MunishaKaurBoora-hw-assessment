7eb23e08e00837171895c45a1614822ff04dfdba
munishaboora <munishabora@hotmail.co.uk>
2022-07-30 05:07:58 +0100

    Wrote additional code for question 2

--- Scratch file.py
+++ Scratch file.py
@@ -1,29 +1,45 @@
-#Polymorphism
-class VehicleDetails:
+# #Polymorphism
+# class VehicleDetails:
+#
+#     def __init__(self, brand, colour, model):
+#         self.brand = brand
+#         self.colour = colour
+#         self.model = model
+#
+#     def print_vehicle_details(self):
+#         print(f"Vehicle brand: {self.brand}")
+#         print(f"Vehicle colour: {self.colour}")
+#         print(f"Vehicle model: {self.model}")
+#
+# class Cycle(VehicleDetails):
+#     def print_vehicle_details(self):
+#         print(f"The cycle's brand is {self.brand} and its model name is {self.model}. The cycle is also {self.colour} in colour.")
+#
+# class Car(VehicleDetails):
+#     def print_vehicle_details(self):
+#         print(f"The car's brand is {self.brand} and its model name is {self.model}. The car is also {self.colour} in colour.")
+#
+#
+# munishas_cycle = Cycle('Raleigh', 'blue', 'stowaway folding bike')
+# munishas_cycle.print_vehicle_details()
+#
+# munishas_car = Car('BMW', 'purple', 'BMW X5')
+# munishas_car.print_vehicle_details()
 
-    def __init__(self, brand, colour, model):
-        self.brand = brand
-        self.colour = colour
-        self.model = model
 
-    def print_vehicle_details(self):
-        print(f"Vehicle brand: {self.brand}")
-        print(f"Vehicle colour: {self.colour}")
-        print(f"Vehicle model: {self.model}")
+class Lion:
+    def diet(self):
+        print("carnivore")
 
-class Cycle(VehicleDetails):
-    def print_vehicle_details(self):
-        print(f"The cycle's brand is {self.brand} and its model name is {self.model}. The cycle is also {self.colour} in colour.")
+class Giraffe:
+    def diet(self):
+        print("herbivore")
 
-class Car(VehicleDetails):
-    def print_vehicle_details(self):
-        print(f"The car's brand is {self.brand} and its model name is {self.model}. The car is also {self.colour} in colour.")
+object_lion=Lion()
+object_lion.diet()
 
+object_giraffe=Giraffe()
+object_giraffe.diet()
 
-munishas_cycle = Cycle('Raleigh', 'blue', 'stowaway folding bike')
-munishas_cycle.print_vehicle_details()
-
-munishas_car = Car('BMW', 'purple', 'BMW X5')
-munishas_car.print_vehicle_details()
 
 
